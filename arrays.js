9b52fce0233e7a346595c08a83a83a37624ba541
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-19 21:47:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,8 +12,9 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
+
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
+  array.push(element)
   return array
 }
 
