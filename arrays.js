da91b437e5009307217fb05cf3c65183a0f6ee94
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-20 00:02:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  console.log(chocolateBars[3])
-  return array
+  return "chocolateBars[3]"
 }
